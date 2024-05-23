558769ce08aac2aa94c78fe7827a1fb20ad98499
vrun545 <vrun545@gmail.com>
2024-05-23 09:13:44 +0530

    Added functionality to generate a new report evrytime run test cases

--- src/main/java/reports/ExtentManager.java
+++ src/main/java/reports/ExtentManager.java
@@ -3,6 +3,9 @@ package reports;
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class ExtentManager {
     private static ExtentReports extent;
     private static ExtentSparkReporter htmlReporter;
@@ -10,7 +13,9 @@ public class ExtentManager {
     public static ExtentReports getInstance() {
         if (extent == null) {
             extent = new ExtentReports();
-            htmlReporter = new ExtentSparkReporter("src/main/java/reports/ExtentReport.html");
+            String timeStamp = new SimpleDateFormat("yyyy-MM-dd_HH-mm-ss").format(new Date());
+            String reportFileName = "src/main/java/reports/ExtentReport_" + timeStamp + ".html";
+            htmlReporter = new ExtentSparkReporter(reportFileName);
             htmlReporter.config().setReportName("Test Report");
             htmlReporter.config().setDocumentTitle("Extent Report");
             htmlReporter.config().setTheme(com.aventstack.extentreports.reporter.configuration.Theme.STANDARD);
